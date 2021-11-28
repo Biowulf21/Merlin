f6c348a0bc1f73b374102da6c1fae1b12555d9b3
James Jilhaney <notail2111@gmail.com>
2021-11-28 16:49:25 +0800

    Full functionality without feedback windows

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ from UI import Ui_MainWindow
 
 import Sheets
 import Subscriber
+import sendMail
+
+#email imports
+
+import smtplib
+from email.message import EmailMessage
+
 
 
 class UI(QMainWindow):
@@ -18,6 +25,7 @@ class UI(QMainWindow):
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.msg = sendMail.SendMessage()
         UI.UpdateEmailTextBox(self, "", "", "")
         self.show()
 
@@ -26,42 +34,48 @@ class UI(QMainWindow):
         self.ui.lNameButton.clicked.connect(self.SearchLastName)
         #print('back in main after last name search')
         self.ui.selectSubButton.clicked.connect(self.SelectSubscriber)
+        self.ui.emailSendButton.clicked.connect(self.sendMessage)
 
     #def UpdateUserInfo(self):
+    #TODO: Refactor and Modularize code
 
+    
        # self.ui.fName.text() = 
     def SearchUserID(self):
         xuMail = self.ui.xuMailLineEdit.text()
-        subscriberData = Sheets.SearchID(xuMail)
-        email = subscriberData[1]
-        fname = subscriberData[2]
-        lname = subscriberData[3]
-        date = subscriberData[4]
-        time = subscriberData[5]
-        phoneNumber = subscriberData[6]
-        status = subscriberData[7]
-        subscriber = Subscriber.Subscriber(fname, lname, email, date, time, phoneNumber, status)
-        self.ui.lNameLineEdit.clear()
-        UI.updateUser(self, subscriber)
-        print("In Search ID")
+        try:
+            subscriberData = Sheets.SearchID(xuMail)
+            email = subscriberData[1]
+            fname = subscriberData[2]
+            lname = subscriberData[3]
+            date = subscriberData[4]
+            time = subscriberData[5]
+            phoneNumber = subscriberData[6]
+            status = subscriberData[7]
+            subscriber = Subscriber.Subscriber(fname, lname, email, date, time, phoneNumber, status)
+            self.ui.lNameLineEdit.clear()
+            UI.updateUser(self, subscriber)
+        except:
+            print("change value")
 
     def SearchLastName(self):
         #TODO: Search Function for last name should not be case sensitive
         lastName = self.ui.lNameLineEdit.text()
-        subscriberData = Sheets.SearchLastName(lastName)
-        email = subscriberData[1]
-        fname = subscriberData[2]
-        lname = subscriberData[3]
-        date = subscriberData[4]
-        time = subscriberData[5]
-        phoneNumber = subscriberData[6]
-        status = subscriberData[7]
-
-        subscriber = Subscriber.Subscriber(fname, lname, email, date, time, phoneNumber, status)
-        #UI.UpdateUserInfo(self)
-        self.ui.xuMailLineEdit.clear()
-        UI.updateUser(self,subscriber)
-        #print("In Search Last Name")
+        try:
+            subscriberData = Sheets.SearchLastName(lastName)
+            email = subscriberData[1]
+            fname = subscriberData[2]
+            lname = subscriberData[3]
+            date = subscriberData[4]
+            time = subscriberData[5]
+            phoneNumber = subscriberData[6]
+            status = subscriberData[7]
+            subscriber = Subscriber.Subscriber(fname, lname, email, date, time, phoneNumber, status)
+            self.ui.xuMailLineEdit.clear()
+            UI.updateUser(self,subscriber)
+        except:
+            print("Change value")
+        
         
 
 
@@ -94,11 +108,53 @@ class UI(QMainWindow):
         self.ui.status.setText(subscriber.getStatus)
 
     def SelectSubscriber(self):
-        name = self.ui.fName.text()
+        fname = self.ui.fName.text()
+        lname = self.ui.lName.text()
         date = self.ui.claimDate.text()
         time = self.ui.claimTime.text()
 
-        self.UpdateEmailTextBox(name, date, time)
+        self.UpdateEmailTextBox(fname, date, time)
+        self.ui.receipentLineEdit.setText(fname + lname)
+
+
+    def sendMessage(self):
+        sender = "crusaderyearbook@xu.edu.ph"
+        password = "uisjoyfegjgudwpp"
+
+        subject = self.ui.subjectLineEdit.text()
+        body = self.ui.emailBodyText.toPlainText()
+        receipient = self.ui.xuMail.text()
+        sendName = self.ui.fName.text()
+        sendLName = self.ui.lName.text()
+
+        #print(str(all) + " recipients found.\n")
+        sent = 0
+        failedTo = []; 
+
+        sendTo = receipient
+        message = EmailMessage()
+        message['Subject'] = subject
+        message['From'] = sender
+        message['To'] = sendTo
+        message.set_content(body)
+
+        try:
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+                smtp.login(sender, password)
+                smtp.send_message(message)
+
+                sent = sent+1
+        except Exception as e:
+            failedTo.append(sendTo)
+        
+        #summary report 
+        print("Message sent to " + receipient + " - " + sendName + sendLName + " - " + receipient)
+        if len(failedTo) != 0:
+            print("Failed to send to: ")
+            print(*failedTo, sep = ", ")
+
+
+
     
 
 
--- sendMail.py
+++ sendMail.py
@@ -2,41 +2,51 @@
 import smtplib
 from email.message import EmailMessage
 
-sender = "crusaderyearbook@xu.edu.ph"
-password = "uisjoyfegjgudwpp"
+from PyQt5.QtWidgets import QMainWindow
+import UI
 
 
-def advisory(name, date, time, receipient):
-    subject = "[Crusader Yearbook] Claiming Yearbook"
-    messagebody = 'Greetings {}, \n\nYour yearbook is ready to claim on {} at {}\n\nIn Service\nThe Crusader Yearbook'.format(name, date, time)
-
-    sendMessage(subject, messagebody, receipient, name)
-
-
-def sendMessage(subject, body, receipient, name):
-    #print(str(all) + " recipients found.\n")
-    sent = 0
-    failedTo = []; 
-
-    sendTo = receipient
-    message = EmailMessage()
-    message['Subject'] = subject
-    message['From'] = sender
-    message['To'] = sendTo
-    message.set_content(body)
-
-    try:
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-            smtp.login(sender, password)
-            smtp.send_message(message)
-
-            sent = sent+1
-    except Exception as e:
-        failedTo.append(sendTo)
+class SendMessage(object):
+        
     
-    #summary report 
-    print("Message sent to " + receipient + " - " + name)
-    if len(failedTo) != 0:
-        print("Failed to send to: ")
-        print(*failedTo, sep = ", ")
+
+
+
+    def sendMessage(self):
+        self.UI = UI.Ui_MainWindow()
+        sender = "crusaderyearbook@xu.edu.ph"
+        password = "uisjoyfegjgudwpp"
+
+        
+
+        subject = self.UI.subjectLineEdit.text()
+        body = self.UI.emailBodyText.toPlainText()
+        receipient = self.UI.xuMail.text()
+        sendName = self.UI.fName.text()
+
+        #print(str(all) + " recipients found.\n")
+        sent = 0
+        failedTo = []; 
+
+        sendTo = receipient
+        message = EmailMessage()
+        message['Subject'] = subject
+        message['From'] = sender
+        message['To'] = sendTo
+        message.set_content(body)
+
+        try:
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
+                smtp.login(sender, password)
+                smtp.send_message(message)
+
+                sent = sent+1
+        except Exception as e:
+            failedTo.append(sendTo)
+        
+        #summary report 
+        print("Message sent to " + receipient + " - " + receipient)
+        if len(failedTo) != 0:
+            print("Failed to send to: ")
+            print(*failedTo, sep = ", ")
 
