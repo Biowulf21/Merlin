9351aa3f28e00c8578e058b741b1c43c9eeb6507
James Jilhaney <notail2111@gmail.com>
2022-03-23 13:47:17 +0800

    updated password in settings file

--- modules/settings.py
+++ modules/settings.py
@@ -1,7 +1,7 @@
 HOST = 'smtp.gmail.com'
 PORT = 587
 SENDER = "crusaderyearbook@xu.edu.ph"
-PASSWORD = "uisjoyfegjgudwpp"
+PASSWORD = "ahryfeqssshfpqsu"
 RECIPIENT = '20190016830@my.xu.edu.ph'
 MESSAGE_FILE = 'compose.md'
 DISPLAY_NAME = 'The Crusader Yearbook 2022'
